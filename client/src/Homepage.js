9a431b3e38237e2fa28f9ea5536c76f86233ac4b
Jason Bracco <bracco.jason@gmail.com>
2023-02-10 17:53:02 -0500

    login and sign up buttons dislaying properly in homepage

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,6 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
+import LoginPage from "./LoginPage"
+import SignupPage from "./SignupPage"
+
 
 function Homepage(){
+
+    const [loggingIn, setLoggingIn] = useState(false)
+    const [signingUp, setSigningUp] = useState(false)
+    const [undo, setUndo] = useState(false)
+
+    function loginClick(){
+        setLoggingIn(true)
+        setSigningUp(false)
+    }
+
+    function signupClick(){
+        setSigningUp(true)
+        setLoggingIn(false)
+    }
+
+    function undoClick(){
+        setLoggingIn(false)
+        setSigningUp(false)
+    }
 
     return(
 
@@ -10,9 +32,28 @@ function Homepage(){
             <p>List Your Work</p>
             <p>Build Your Collection</p>  
             <div>
-                <button className="login-button">Login</button> 
-                <button className="signin-button">Sign Up</button>
+                <button onClick={loginClick} className="login-button">Login</button> 
             </div>
+            <div>
+                <button onClick={signupClick} className="signup-button">Sign Up</button>
+            </div>
+            {loggingIn ?
+                <div>
+                    <LoginPage />
+                    <button onClick={undoClick}>Undo</button>
+                </div>
+            :
+            null
+            }
+            {signingUp ? 
+                <div>
+                    <SignupPage />
+                    <button onClick={undoClick}>Undo</button>
+                </div>
+            :
+            null
+            }
+
         </div>
     )
 }
