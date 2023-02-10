bf5867afa5933bdf9dc28f4b80efc9b181fe8124
Jason Bracco <bracco.jason@gmail.com>
2023-02-09 22:05:53 -0500

    setting up bones for homepage

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -2,11 +2,17 @@ import React from "react"
 
 function Homepage(){
 
-
     return(
 
         <div>
-            <p>This is the homepage, welcome!</p>
+            <p>Welcome to PhotoShare</p>
+            <p>Discover New Artists</p>
+            <p>List Your Work</p>
+            <p>Build Your Collection</p>  
+            <div>
+                <button className="login-button">Login</button> 
+                <button className="signin-button">Sign Up</button>
+            </div>
         </div>
     )
 }
