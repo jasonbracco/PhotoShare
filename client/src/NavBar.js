924a40a7e538546f178233127d689f0240611d22
Jason Bracco <bracco.jason@gmail.com>
2023-02-09 19:25:27 -0500

    active storage migration completed

---
+++ client/src/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+function NavBar(){
+
+    return(
+        <div>
+            <div className="Navbar">
+                <p>Profile</p>
+                <p>Shop</p>
+                <p>Orders</p>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
