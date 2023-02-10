d7f0a4a5ec40d3d9a701b6132172fc8141860cb8
Jason Bracco <bracco.jason@gmail.com>
2023-02-09 19:43:02 -0500

    Putting links into navbar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,9 +6,19 @@ function NavBar(){
     return(
         <div>
             <div className="Navbar">
-                <p>Profile</p>
-                <p>Shop</p>
-                <p>Orders</p>
+                <p className="NavBar-image"> Image here</p>
+                <div className="NavBar-options">
+                    <Link to="/" className="nav-link">Homepage</Link>
+                    <br></br>
+                    <br></br>
+                    <Link to="/profile" className="nav-link">Profile</Link>
+                    <br></br>
+                    <br></br>
+                    <Link to="/shop" className="nav-link">Shop</Link>
+                    <br></br>
+                    <br></br>
+                    <Link to="/orders" className="nav-link">Orders</Link>
+                </div>
             </div>
         </div>
     )
