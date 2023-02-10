64f3cc6f833ec9d26ad726d62d6703b6e8025cb2
Jason Bracco <bracco.jason@gmail.com>
2023-02-09 20:22:37 -0500

    adding in profile route

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import NavBar from "./NavBar"
 import {Route, Routes} from "react-router-dom"
 import Homepage from "./Pages/Homepage"
+import Profile from "./Pages/Profile"
 
 function App() {
 
-
-
-
-
-
   return (
     <div>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
+      <div className="header">Header</div>
+      <div>Cart</div>
+      <div className="side-navbar">
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </div>
   );
 }
