600e3df0ff2ccab7c29911f5bdc7c38a1353ef32
Jason Bracco <bracco.jason@gmail.com>
2023-02-11 11:43:02 -0500

    user controller signup post method created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    #post: signup
+    def create
+        user=User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
 end
