53c9c29c62fc26cbd52fe0db4d0459b0239b9cd3
Jason Bracco <bracco.jason@gmail.com>
2023-02-11 13:00:37 -0500

    fixed typo in application controller and dropped/created/migrated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::
   
-  rescue from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   before_action :authorize
 
