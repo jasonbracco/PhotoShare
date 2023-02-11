de1e26a787d94782d6532871edb6856e6feac180
Jason Bracco <bracco.jason@gmail.com>
2023-02-11 11:45:04 -0500

    strong params added to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,11 @@ class UsersController < ApplicationController
         session[:user_id] = user.id
         render json: user, status: :created
     end
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :city, :state_province, :country, :bio, :username, :password, :password_confirmation, :image)
+    end
+    
 end
