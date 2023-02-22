6bacd36ecad28862153502a21bd6a0b02df66509
Jason Bracco <bracco.jason@gmail.com>
2023-02-21 22:43:26 -0500

    configuring production.rb to have the correct url and require master key

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -10,7 +10,7 @@ function Profile(){
 
     return (
         <div>
-            {user.id}
+            Image: 
         </div>
     )
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -110,4 +110,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: "https://photo-share-app-b2q6.onrender.com" }
+  Rails.application.routes.default_url_options[:host] = "https://photo-share-app-b2q6.onrender.com"
+
+
 end
