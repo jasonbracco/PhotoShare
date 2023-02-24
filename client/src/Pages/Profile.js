18ceb553f2dd8dc1adc67630c04d74350a833ce5
Jason Bracco <bracco.jason@gmail.com>
2023-02-23 19:12:43 -0500

    building out bones for profile page

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -22,7 +22,14 @@ function Profile(){
 
     return (
         <div>
+            <button className="list-work-button">List Work</button>
             <img className="profile-pic" src={user.image} />
+            <div classame="user-bio">
+                {user.bio}
+            </div>
+            <div className="peronal-listed-items">
+                All of the works that I am selling will go here
+            </div>
         </div>
     )
 }
