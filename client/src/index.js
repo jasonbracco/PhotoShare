a87436b86fd6af6ef1cf70f280a86633f76f2caa
Jason Bracco <bracco.jason@gmail.com>
2023-03-23 23:56:31 -0400

    still trying to render properly

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom" 
-import 'semantic-ui-css/semantic.min.css'
 import './index.css'; 
 import App from './App'; 
+import 'semantic-ui-css/semantic.min.css'
+
 
 ReactDOM.render(
   <BrowserRouter> 
